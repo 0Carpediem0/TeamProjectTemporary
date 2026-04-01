cc2c9697205a37bd91ef73dae8c75ccb787beeb0
Tigran Petrosyan <tigr-080807@mail.ru>
2026-04-01 20:28:34 +0500

    Написание алгоритма оценки сложности пароля

---
+++ backend/utils/check_breached_password.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from backend.models.passwords import BreachedHash
+from .hash import hash_password
+
+
+async def check_breached_password(db: AsyncSession, password: str) -> bool:
+    hashed_password = hash_password(password)
+    breached_password = (await db.scalars(
+        select(BreachedHash).where(BreachedHash.hash_password == hashed_password)
+    )).first()
+    if breached_password is not None:
+        return True
+    return False
