d87f4809d7b5c681de4810eb5f3d6ef1849deff3
Tigran Petrosyan <tigr-080807@mail.ru>
2026-04-23 15:25:28 +0500

    Доработки возвращаемого значения функции проверки на содержание в базе утечек

--- backend/utils/rules/breach.py
+++ backend/utils/rules/breach.py
@@ -1,14 +1,20 @@
+"""
+Модуль для проверки пароля по базе утечек.
+В учебном проекте обращается к базе данных через сессию SQLAlchemy.
+"""
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from backend.models.passwords import BreachedHash
 from backend.utils.hash import hash_password
 
 
-async def check_breached_password(db: AsyncSession, password: str) -> bool:
+async def is_password_breached(db: AsyncSession, password: str) -> bool:
+    """
+    Проверяет, найден ли пароль в базе утечек (по SHA-256 хэшу).
+    """
     hashed_password = hash_password(password)
     breached_password = (await db.scalars(
         select(BreachedHash).where(BreachedHash.hash_password == hashed_password)
     )).first()
-    if breached_password is not None:
-        return True
-    return False
+    return breached_password is not None
